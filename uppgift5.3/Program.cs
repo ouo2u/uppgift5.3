1c6a12eff6d36faa112d156142155ff2bc1ee326
ouo2u <abod98451@gmail.com>
2023-02-01 00:01:48 +0100

    Add project files.

---
+++ uppgift5.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uppgift5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
