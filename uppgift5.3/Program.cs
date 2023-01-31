240a0f25116567cb99f8516dbbba8b8b85e5197a
ouo2u <111962660+ouo2u@users.noreply.github.com>
2023-02-01 00:02:38 +0100

    Update Program.cs

--- uppgift5.3/Program.cs
+++ uppgift5.3/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 
-namespace uppgift5._3
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] fraga = { "Vad heter frankrikes huvudstad?", "Vem vann melodifestivalen 2015?", "Vad firar svenskar på midsommar?", "Vad heter världens största djur?" };
+            string[] rätt = { "Paris", "Måns Zelmerlöw", "Midsommar", "Blåval" };
+
+            bool kör = true;
+
+            while (kör = true)
+            {
+                Console.WriteLine("\nVälj mellan fråga 1-4");
+                int val = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\n\nFråga {val}");
+                Console.WriteLine(fraga[val - 1]);
+                string svar = Console.ReadLine();
+
+                if (svar == rätt[val - 1])
+                {
+                    Console.WriteLine("\nRätt!!!\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nFel >:(\n");
+                }
+
+                Console.WriteLine("Vill du fortsätta? y/n");
+                string fortsatta = Console.ReadLine();
+                if (fortsatta == "y")
+                {
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+            kör = false;
         }
     }
 }
